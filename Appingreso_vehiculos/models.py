fee8bbb76e0f8d183ad88eb1566effc97ff9f7c3
Medinx44 <medinamartinezk@gmail.com>
2025-11-12 14:59:50 -0500

    0 models
    sin models

--- Appingreso_vehiculos/models.py
+++ Appingreso_vehiculos/models.py
@@ -1,12 +1,2 @@
 # Appingreso_vehiculos/models.py
 from django.db import models
-from django.utils import timezone
-from Appregistro.models import Vehiculo   # 👈 importamos el modelo de Appregistro
-
-class IngresoVehiculo(models.Model):
-    vehiculo = models.ForeignKey(Vehiculo, on_delete=models.CASCADE, related_name="ingresos")
-    fecha_entrada = models.DateTimeField(default=timezone.now)
-    fecha_salida = models.DateTimeField(null=True, blank=True)
-
-    def __str__(self):
-        return f"Ingreso {self.vehiculo.placa} - {self.fecha_entrada.strftime('%d/%m/%Y %H:%M')}"
