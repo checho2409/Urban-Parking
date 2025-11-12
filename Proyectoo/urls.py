c0c4fc8d287718ec3965187e2b219b6b6cfa9b8c
Medina444 <medinamartinezk@gmail.com>
2025-11-11 22:53:05 -0500

    Cambie botones de contacto y ya
    cambio botones de contacto para redireccionar a nosotros

--- Proyectoo/urls.py
+++ Proyectoo/urls.py
@@ -5,12 +5,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # apps
-    path("", include("Appingreso.urls")),   # login, signup, logout
-    path('', include('Appinicio.urls')),            # inicio
+    path("", include("Appingreso.urls")),  
+    path('', include('Appinicio.urls')),      
     path('informacion/', include('Appinformacion.urls')),
     path('historial/', include('Apphistorial.urls')), 
     path('pagos/', include('Apppagos.urls')),
-    path('registro/', include('Appregistro.urls')), # 👈 mejor también inclúyelo
+    path('registro/', include('Appregistro.urls')),
     path("iniciosoft/", include("Appiniciosoft.urls")),
     path("ingresos-vehiculos/", include("Appingreso_vehiculos.urls")),
     path("nosotros/", include("Appnosotros.urls")),
